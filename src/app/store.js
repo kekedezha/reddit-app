2f3724bdc30bf04bd3799dbeb39e337dc54be9b0
kekedezha <dezhac6@gmail.com>
2024-07-11 16:41:57 -0400

    set up app/store.js directory

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+// store.js
+import { configureStore } from "@reduxjs/toolkit";
+// import reducer functions from slices created to configure store
+
+// Store configuration with all slices created.
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
