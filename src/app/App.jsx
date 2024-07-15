bb05505443c06a141f147c7535d188210f540c71
kekedezha <dezhac6@gmail.com>
2024-07-15 17:45:09 -0400

    updated import statements for main and App .jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import Header from "../features/Header/Header.jsx";
+import Home from "../features/Home/Home.jsx";
+import Subreddits from "../features/Subreddits/Subreddits.jsx";
+
+import "../App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Home />
+      </main>
+      <aside>
+        <Subreddits />
+      </aside>
+    </>
+  );
 }
 
 export default App;
