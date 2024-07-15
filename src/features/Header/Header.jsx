92091779c283237fb5258d88be3023c5c01fcf6d
kekedezha <dezhac6@gmail.com>
2024-07-15 18:07:54 -0400

    updated css file for Header comp

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -1,7 +1,28 @@
 import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="logo">
+        <p className="logo-icon">Logo will go here</p>
+        <p>
+          <span>sub</span>Reddit App
+        </p>
+      </div>
+      <form className="search" onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          placeholder="Search"
+          value={3}
+          onChange={() => {}}
+          aria-label="Search Posts"
+        />
+        <button type="submit" onClick={() => {}} aria-label="Search">
+          Search
+        </button>
+      </form>
+    </header>
+  );
 };
 
 export default Header;
