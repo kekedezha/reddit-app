e43e03105eb01cf34b6b677af5956bfbd769e8fb
kekedezha <dezhac6@gmail.com>
2024-07-17 19:07:52 -0400

    completed avatar component.

--- src/features/Avatar/Avatar.jsx
+++ src/features/Avatar/Avatar.jsx
@@ -1,7 +1,13 @@
 import "./Avatar.css";
 
-const Avatar = () => {
-  return <div>Avatar</div>;
+const Avatar = ({ name }) => {
+  return (
+    <img
+      src={`https://api.adorable.io/avatars/10/${name}`}
+      alt={`${name} profile`}
+      className="avatar-profile-image"
+    />
+  );
 };
 
 export default Avatar;
