7e284848f5533ac32032a566cc176600cda91997
kekedezha <dezhac6@gmail.com>
2024-07-18 12:21:25 -0400

    completed comment component

--- src/features/Comment/Comment.jsx
+++ src/features/Comment/Comment.jsx
@@ -1,7 +1,21 @@
+import moment from "moment";
+import ReactMarkdown from "react-markdown";
+import Avatar from "../Avatar/Avatar";
 import "./Comment.css";
 
-const Comment = () => {
-  return <div>Comment</div>;
+const Comment = ({ comment }) => {
+  return (
+    <div className="comment">
+      <div className="comment-metadata">
+        <Avatar name={comment.author} />
+        <p className="comment-author">{comment.author}</p>
+        <p className="comment-created-time">
+          {moment.unix(comment.created_utc).fromNow()}
+        </p>
+      </div>
+      <ReactMarkdown source={comment.body} />
+    </div>
+  );
 };
 
 export default Comment;
