ef9201bd0b2117de8179285284dd8bf36e451e49
kekedezha <dezhac6@gmail.com>
2024-07-19 22:44:53 -0400

    updated slice with thunk to fetch posts and to filter through posts as well

--- src/features/reddit/redditSlice.js
+++ src/features/reddit/redditSlice.js
@@ -1,5 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import {
+  createSlice,
+  createAsyncThunk,
+  createSelector,
+} from "@reduxjs/toolkit";
+import { getSubredditPosts, getPostComments } from "../../api/reddit";
 const initialState = {
   isLoading: false,
   posts: [],
@@ -18,21 +22,55 @@ const redditSlice = createSlice({
     setPosts: (state, action) => {
       state.posts = action.payload;
     },
-    postsLoading: (state) => {
-      state.isLoading = true;
-      state.error = false;
-    },
-    postsLoadingSuccess: (state, action) => {
-      state.isLoading = false;
-      state.posts = action.payload;
-    },
-    postsLoadingFail: (state) => {
-      state.isLoading = false;
-      state.error = true;
-    },
     setSelectedSubreddit: (state, action) => {
       state.selectedSubreddit = action.payload;
     },
+    toggleShowingComments(state, action) {
+      state.posts[action.payload].showingComments =
+        !state.posts[action.payload].showingComments;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.isLoading = true;
+        state.error = false;
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.posts = action.payload;
+        state.posts = state.posts.map((post) => ({
+          ...post,
+          showingComments: false,
+          comments: [],
+          loadingComments: false,
+          errorComments: false,
+        }));
+      })
+      .addCase(fetchPosts.rejected, (state) => {
+        state.isLoading = false;
+        state.error = false;
+      })
+      .addCase(fetchComments.pending, (state, action) => {
+        const index = action.meta.arg.index;
+        state.posts[index].showingComments =
+          !state.posts[index].showingComments;
+        if (!state.posts[index].showingComments) {
+          return;
+        }
+        state.posts[index].loadingComments = true;
+        state.posts[index].errorComments = false;
+      })
+      .addCase(fetchComments.fulfilled, (state, action) => {
+        const { index, comments } = action.payload;
+        state.posts[index].loadingComments = false;
+        state.posts[index].comments = comments;
+      })
+      .addCase(fetchComments.rejected, (state, action) => {
+        const index = action.payload.index;
+        state.posts[index].loadingComments = false;
+        state.posts[index].errorComments = true;
+      });
   },
 });
 
@@ -46,5 +84,39 @@ export const {
   setSelectedSubreddit,
 } = redditSlice.actions;
 
+export const fetchPosts = createAsyncThunk(
+  "reddit/fetchPosts",
+  async (subreddit) => {
+    return await getSubredditPosts(subreddit);
+  }
+);
+
+export const fetchComments = createAsyncThunk(
+  "reddit/fetchComments",
+  async ({ index, permalink }, { rejectWithValue }) => {
+    try {
+      const comments = await getPostComments(permalink);
+      return { index, comments };
+    } catch (error) {
+      return rejectWithValue({ index, error: error.message });
+    }
+  }
+);
+
 export const selectSelectedSubreddit = (state) =>
   state.reddit.selectedSubreddit;
+const selectPosts = (state) => state.reddit.posts;
+const selectSearchTerm = (state) => state.reddit.searchTerm;
+
+export const selectFilteredPosts = createSelector(
+  [selectPosts, selectSearchTerm],
+  (posts, searchTerm) => {
+    if (searchTerm !== "") {
+      return posts.filter((post) =>
+        post.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+
+    return posts;
+  }
+);
