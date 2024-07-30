1bc396f7e4581a800a5bc930234d831ade610b29
kekedezha <dezhac6@gmail.com>
2024-07-29 22:09:05 -0400

    updated test for header comp, still need to complete the last test in file

--- tests/features/Header/headerComponent.test.jsx
+++ tests/features/Header/headerComponent.test.jsx
@@ -1,5 +1,50 @@
+import Header from "../../../src/features/Header/Header";
+import { Provider } from "react-redux";
+import store from "../../../src/app/store";
+
+import { beforeEach } from "vitest";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
 describe("Header component", () => {
-  test("loads and displays proper elements", () => {
-    expect(1).toEqual(1);
+  test("loads and displays logo", () => {
+    const { container } = render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+    const logo = container.getElementsByClassName("logo");
+    expect(logo[0]).toHaveTextContent("subReddit App");
+  });
+
+  test("loads and displays app description", () => {
+    const { container } = render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+    const description = container.getElementsByClassName("app-description");
+    expect(description[0]).toHaveTextContent(
+      "A sub(stitute) to the Reddit app"
+    );
+  });
+
+  test("loads and displays search bar", () => {
+    const { container } = render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+    const searchBar = container.querySelector("input");
+    expect(searchBar).toBeInTheDocument();
+  });
+
+  test("search bar updates state", () => {
+    const { container } = render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+    const searchBar = container.querySelector("input");
   });
 });
